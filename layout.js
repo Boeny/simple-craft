9dca58cdcdd5d502eb440413fcdf8ded32172bfa
Boeny <boeniareadmirr@gmail.com>
2017-03-20 15:35:14 +0300

    layout external url

--- layout.js
+++ layout.js
@@ -1,23 +1,24 @@
 var h = require('./html');
 var fs = require('fs');
+var external = 
+'https://code.jquery.com/jquery-3.0.0.min.js';
 
 function createModule(name){
 	return '(function(global){var module = {exports: {}};'+fs.readFileSync(__dirname+'/'+name+'.js', 'utf-8')+'return module.exports;})(window);';
 }
 
 module.exports = function(content, response){
-	__server.requireUrl('https://code.jquery.com/jquery-3.0.0.min.js', function(remoteSrc){
-		response.end(h.type+
-			h.html(
-				h.head(
-					h.getMetas()+
-					h.style()+
-					h.script(remoteSrc)+
-					h.script(createModule('base'))+
-					h.script('var h = '+createModule('html'))
-				)+
-				h.body(content)
-			)
-		);
-	});
+	response.end(h.type+
+		h.html(
+			h.head(
+				h.getMetas()+
+				h.style()+
+				h.script({src: 
+external})+
+				h.script(createModule('base'))+
+				h.script('var h = '+createModule('html'))
+			)+
+			h.body(content)
+		)
+	);
 };
